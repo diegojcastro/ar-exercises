a1960af8a7aca66aef91522b5db2ce76d0b44545
Diego Castro <diegojcr4@gmail.com>
2021-10-11 16:57:59 -0400

    add code requirements for exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total = Store.sum(:annual_revenue)
+num_stores = Store.count
+puts "Total revenue is #{total}"
+puts "Average for all stores is #{total/num_stores}"
+puts ""
+stores_for_5 = Store.where("annual_revenue > 1000000").count
+puts "Stores that are generating $1M or more in annual sales: #{stores_for_5}"
